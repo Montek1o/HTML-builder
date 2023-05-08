2e9ee054ca08b8ae423445a40654b360bf503f30
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-05-08 22:11:05 +0300

    feat: complete task1

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const stream = fs.createReadStream(path.resolve('01-read-file', 'text.txt'), 'utf-8');
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
